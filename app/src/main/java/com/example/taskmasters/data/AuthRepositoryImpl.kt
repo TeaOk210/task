e0a18e69accca499153e521009cf8b09dd9316e2
TeaOk210 <danilplay21006@gmail.com>
2023-07-07 14:42:50 +0200

    обработка исключений

--- app/src/main/java/com/example/taskmasters/data/AuthRepositoryImpl.kt
+++ app/src/main/java/com/example/taskmasters/data/AuthRepositoryImpl.kt
@@ -1,9 +1,10 @@
 package com.example.taskmasters.data
 
-import android.util.Log
 import com.example.taskmasters.util.Resource
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthInvalidUserException
 import com.google.firebase.auth.FirebaseAuthUserCollisionException
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
@@ -11,7 +12,6 @@ import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.tasks.await
 
 class AuthRepositoryImpl(
     private val auth: FirebaseAuth = Firebase.auth
@@ -26,18 +26,35 @@ class AuthRepositoryImpl(
                         val result = task.result
                         deferred.complete(Resource.Success(result))
                     } else {
-                        val errorMessage = task.exception?.message ?: "Unknown error"
-                        deferred.complete(Resource.Error(errorMessage))
+                        val exception = task.exception
+                        val errorMessage = exception?.message ?: "Неизвестная ошибка"
+                        when (exception) {
+                            is FirebaseAuthInvalidUserException -> {
+                                deferred.complete(Resource.Error("Пользователь не найден или удален"))
+                            }
+
+                            is FirebaseAuthInvalidCredentialsException -> {
+                                deferred.complete(Resource.Error("Неправильный пароль"))
+                            }
+
+                            is FirebaseAuthUserCollisionException -> {
+                                deferred.complete(Resource.Error("Учетная запись уже используется"))
+                            }
+
+                            else -> {
+                                deferred.complete(Resource.Error(errorMessage))
+                            }
+                        }
                     }
                 }
             try {
-                Log.d("Result::::::::::::::::::::", deferred.await().toString())
                 emit(deferred.await())
             } catch (e: Exception) {
                 emit(Resource.Error(e.message.toString()))
             }
         }
     }
+
 
     override fun registrUser(email: String, password: String): Flow<Resource<AuthResult>> {
         return flow {
@@ -49,8 +66,12 @@ class AuthRepositoryImpl(
                         val result = task.result
                         deferred.complete(Resource.Success(result))
                     } else {
-                        val errorMessage = task.exception?.message ?: "Unknown error"
-                        deferred.complete(Resource.Error(errorMessage))
+                        val errorMessage = task.exception?.message ?: "Неизвестная ошибка"
+                        if (task.exception is FirebaseAuthUserCollisionException) {
+                            deferred.complete(Resource.Error("Данный адрес уже зарегестрирован"))
+                        } else {
+                            deferred.complete(Resource.Error(errorMessage))
+                        }
                     }
                 }
             try {
